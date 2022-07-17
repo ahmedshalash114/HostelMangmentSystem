cb31ac119ca97a9ca280bcc7718bc5a74a8e5b3e
ahmed shalash <ashalash38@gmail.com>
2022-07-17 19:40:41 +0200

    update hostel mangement system

---
+++ src/main/java/com/example/hostelmangmentsystem/HostelMangmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.hostelmangmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostelMangmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HostelMangmentSystemApplication.class, args);
+    }
+
+}
